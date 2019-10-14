f81201227c6cce5f39d9f38b8baebdb68055a14a
Brennan Newton <brennanmnewton@gmail.com>
2019-10-13 19:29:00 -0700

    Init 3D concept

--- react-bndesign/src/App.js
+++ react-bndesign/src/App.js
@@ -5,20 +5,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Organic</h1>
+      <p>These mushrooms are organic. That means humans haven't exerted their need for control over them... Or have they?</p>
+      <p>Left click to rotate. Right click to pan. Scroll to zoom.</p>
+      <iframe id="8a99778b-92f7-485b-abf6-3e92f2a1c8d2" src="https://www.vectary.com/viewer/v1/?model=8a99778b-92f7-485b-abf6-3e92f2a1c8d2&env=piazzasanmarco" frameborder="0" width="50%" height="400"></iframe>
     </div>
   );
 }
