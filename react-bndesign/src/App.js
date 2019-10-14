a6a6259fc81f3b99f13045cf4fc86ec8a5b51056
Brennan Newton <brennanmnewton@gmail.com>
2019-10-13 19:39:08 -0700

    iframe title property

--- react-bndesign/src/App.js
+++ react-bndesign/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <h1>Organic</h1>
       <p>These mushrooms are organic. That means humans haven't exerted their control over them... Or have they?</p>
       <p>Left click to rotate. Right click to pan. Scroll to zoom.</p>
-      <iframe id="8a99778b-92f7-485b-abf6-3e92f2a1c8d2" src="https://www.vectary.com/viewer/v1/?model=8a99778b-92f7-485b-abf6-3e92f2a1c8d2&env=piazzasanmarco" frameborder="0" width="50%" height="400"></iframe>
+      <iframe title="shrooms" id="8a99778b-92f7-485b-abf6-3e92f2a1c8d2" src="https://www.vectary.com/viewer/v1/?model=8a99778b-92f7-485b-abf6-3e92f2a1c8d2&env=piazzasanmarco" frameborder="0" width="50%" height="400"></iframe>
     </div>
   );
 }
