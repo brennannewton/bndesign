948978213395cee2972c981948c31225cca12a0f
Brennan Newton <brennanmnewton@gmail.com>
2019-10-24 20:07:46 -0700

    responsive navbar

--- react-bndesign/src/App.js
+++ react-bndesign/src/App.js
@@ -6,16 +6,12 @@ import Navigation from './components/Navigation';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <h1>Organic</h1>
-      <p>These mushrooms are organic. That means humans haven't exerted their control over them... Or have they?</p>
-      <p>Left click to rotate. Right click to pan. Scroll to zoom.</p>
-      <iframe title="shrooms" id="8a99778b-92f7-485b-abf6-3e92f2a1c8d2" src="https://www.vectary.com/viewer/v1/?model=8a99778b-92f7-485b-abf6-3e92f2a1c8d2&env=piazzasanmarco" frameborder="0" width="50%" height="400"></iframe>
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ react-bndesign/src/components/Home.js
@@ -0,0 +1,15 @@
+//Components
+import React from 'react';
+
+//Styles
+import './Home.css'
+
+export default class Home extends React.Component {
+  render() {
+    return (
+      <div className="Home">
+        <h1>WELCOME</h1>
+      </div>
+    )
+  }
+}
