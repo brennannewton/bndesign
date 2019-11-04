51bd9f0a462c3169babb17d0b34005f4b48c3295
Brennan Newton <brennanmnewton@gmail.com>
2019-11-04 15:46:56 -0800

    Fix responsive styling box model

--- react-bndesign/src/components/Home.js
+++ react-bndesign/src/components/Home.js
@@ -10,21 +10,19 @@ export default class Home extends React.Component {
   render() {
     return (
       <div className="Home">
-        <Row className="justify-content-center w-100">
-          <Col className="title m-auto p-0 w-100">WELCOME</Col>
+        <Row className="justify-content-center m-auto p-2 w-100">
+          <Col className="title">WELCOME</Col>
         </Row>
-        <h4 className="subtitle mt-3">Please explore.</h4>
-        <div>
-          <Row className="justify-content-center mb-0 w-100">
-            <Col xs={8} md={4} className="mt-5 ml-5 mr-5 mb-0 p-5 bio">Hi, my name is Brennan Newton.
-              I'm a web developer, and this is my work.
-              <br />
-              <br />
-              Click on the icons top right to explore my work and contact me.
-            </Col>
-            <Col xs={8} md={4} className="mt-5 ml-5 mr-5 mb-0 p-5 ux">User Experience (UX)</Col>
-          </Row>
-        </div>
+        <h4 className="subtitle mt-3 mb-3">Please explore.</h4>
+        <Row className="justify-content-center m-auto mb-0 p-4 w-100">
+          <Col xs={8} md={4} className="mt-0 ml-5 mr-5 p-5 bio">Hi, my name is Brennan Newton.
+            I'm a web developer, and this is my work.
+            <br />
+            <br />
+            Click on the icons top right to explore my work and contact me.
+          </Col>
+          <Col xs={8} md={4} className="mt-0 ml-5 mr-5 mb-0 p-5 ux">User Experience (UX)</Col>
+        </Row>
         <Row className="justify-content-center m-auto w-100">
           <Col xs={8} md={3}><iframe id="cd0aa127-4bfa-466e-8157-b2fd198cbe53" src="https://www.vectary.com/viewer/v1/?model=cd0aa127-4bfa-466e-8157-b2fd198cbe53&env=studio1" frameborder="0" width="100%" height="360"></iframe></Col>
           <Col xs={8} md={3}><iframe id="cd0aa127-4bfa-466e-8157-b2fd198cbe53" src="https://www.vectary.com/viewer/v1/?model=cd0aa127-4bfa-466e-8157-b2fd198cbe53&env=studio1" frameborder="0" width="100%" height="360"></iframe></Col>
