8f7b1a25c8bd4f2125a8d609efd4768c1552eddd
Brennan Newton <brennanmnewton@gmail.com>
2019-11-04 15:17:42 -0800

    Add m-auto to 3d container

--- react-bndesign/src/components/Home.js
+++ react-bndesign/src/components/Home.js
@@ -25,7 +25,7 @@ export default class Home extends React.Component {
             <Col xs={8} md={4} className="mt-5 ml-5 mr-5 mb-0 p-5 ux">User Experience (UX)</Col>
           </Row>
         </div>
-        <Row className="justify-content-center align-items-center w-100">
+        <Row className="justify-content-center m-auto w-100">
           <Col xs={8} md={3}><iframe id="cd0aa127-4bfa-466e-8157-b2fd198cbe53" src="https://www.vectary.com/viewer/v1/?model=cd0aa127-4bfa-466e-8157-b2fd198cbe53&env=studio1" frameborder="0" width="100%" height="360"></iframe></Col>
           <Col xs={8} md={3}><iframe id="cd0aa127-4bfa-466e-8157-b2fd198cbe53" src="https://www.vectary.com/viewer/v1/?model=cd0aa127-4bfa-466e-8157-b2fd198cbe53&env=studio1" frameborder="0" width="100%" height="360"></iframe></Col>
           <Col xs={8} md={3}><iframe id="cd0aa127-4bfa-466e-8157-b2fd198cbe53" src="https://www.vectary.com/viewer/v1/?model=cd0aa127-4bfa-466e-8157-b2fd198cbe53&env=studio1" frameborder="0" width="100%" height="360"></iframe></Col>
