4961ae02382d54d8e98955272e1fc4d0a6508c07
Brennan Newton <brennanmnewton@gmail.com>
2019-11-04 15:09:07 -0800

    3d object responsive styling

--- react-bndesign/src/components/Home.js
+++ react-bndesign/src/components/Home.js
@@ -25,11 +25,11 @@ export default class Home extends React.Component {
             <Col xs={8} md={4} className="mt-5 ml-5 mr-5 mb-0 p-5 ux">User Experience (UX)</Col>
           </Row>
         </div>
-        <div className="vectary-container">
-          <iframe id="cd0aa127-4bfa-466e-8157-b2fd198cbe53" src="https://www.vectary.com/viewer/v1/?model=cd0aa127-4bfa-466e-8157-b2fd198cbe53&env=studio1" frameborder="0" width="33%" height="360"></iframe>
-          <iframe id="cd0aa127-4bfa-466e-8157-b2fd198cbe53" src="https://www.vectary.com/viewer/v1/?model=cd0aa127-4bfa-466e-8157-b2fd198cbe53&env=studio1" frameborder="0" width="33%" height="480"></iframe>
-          <iframe id="cd0aa127-4bfa-466e-8157-b2fd198cbe53" src="https://www.vectary.com/viewer/v1/?model=cd0aa127-4bfa-466e-8157-b2fd198cbe53&env=studio1" frameborder="0" width="33%" height="360"></iframe>
-        </div>
+        <Row className="justify-content-center align-items-center w-100">
+          <Col xs={8} md={3}><iframe id="cd0aa127-4bfa-466e-8157-b2fd198cbe53" src="https://www.vectary.com/viewer/v1/?model=cd0aa127-4bfa-466e-8157-b2fd198cbe53&env=studio1" frameborder="0" width="100%" height="360"></iframe></Col>
+          <Col xs={8} md={3}><iframe id="cd0aa127-4bfa-466e-8157-b2fd198cbe53" src="https://www.vectary.com/viewer/v1/?model=cd0aa127-4bfa-466e-8157-b2fd198cbe53&env=studio1" frameborder="0" width="100%" height="480"></iframe></Col>
+          <Col xs={8} md={3}><iframe id="cd0aa127-4bfa-466e-8157-b2fd198cbe53" src="https://www.vectary.com/viewer/v1/?model=cd0aa127-4bfa-466e-8157-b2fd198cbe53&env=studio1" frameborder="0" width="100%" height="360"></iframe></Col>
+        </Row>
       </div>
     )
   }
