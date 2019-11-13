0c327340930cba5ef0d9845d360514c86c687cf7
Brennan Newton <brennanmnewton@gmail.com>
2019-11-13 12:43:04 -0800

    Add alt text to nav icons for accessibility

--- react-bndesign/src/components/js/Navigation.js
+++ react-bndesign/src/components/js/Navigation.js
@@ -20,7 +20,7 @@ export default class Navigation extends React.Component {
         <div id="reactstrap-navbar" className={`${this.state.currPage}`}>
           <Navbar collapseOnSelect expand="md" className="w-100 p-4">
             <Navbar.Brand as={Link} to="/" className="mb-auto" onClick={() => this.props.setCurrPage('Home')}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 20 17">
+              <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 20 17" alt="Link to the Home page">
                 <path id="ic_home_24px" d="M10,20V14h4v6h5V12h3L12,3,2,12H5v8Z" transform="translate(-2 -3)"/>
               </svg>
             </Navbar.Brand>
@@ -28,22 +28,22 @@ export default class Navigation extends React.Component {
               <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
               <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav.Link as={Link} to="music-videos" onClick={() => this.props.setCurrPage('MusicVideos')}>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 68.129 68.129">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 68.129 68.129" alt="Link to the Music Videos page">
                     <path id="music-videos-icon" d="M29.495,21.925V52.2l18.925-15.14ZM63.559,3H10.57A7.592,7.592,0,0,0,3,10.57V63.559a7.592,7.592,0,0,0,7.57,7.57H63.559a7.592,7.592,0,0,0,7.57-7.57V10.57A7.592,7.592,0,0,0,63.559,3Zm0,60.559H10.57V10.57H63.559Z" transform="translate(-3 -3)"/>
                   </svg>
                 </Nav.Link>
                 <Nav.Link as={Link} to="illustrations" onClick={() => this.props.setCurrPage('Illustrations')}>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="41" height="41" viewBox="0 0 68.129 68.129">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="41" height="41" viewBox="0 0 68.129 68.129" alt="Link to the Illustrations page">
                     <path id="illustrations-icon" d="M3,56.935V71.126H17.192L59.047,29.271,44.856,15.079ZM70.022,18.3a3.769,3.769,0,0,0,0-5.336L61.166,4.1a3.769,3.769,0,0,0-5.336,0L48.9,11.03,63.1,25.221,70.022,18.3Z" transform="translate(-3 -2.998)"/>
                   </svg>
                 </Nav.Link>
                 <Nav.Link as={Link} to="ux" onClick={() => this.props.setCurrPage('UX')}>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 93.677 68.129">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 93.677 68.129" alt="Link to the User Experience page">
                     <path id="ux-icon" d="M9.516,12.516H86.161V4H9.516A8.541,8.541,0,0,0,1,12.516v51.1a8.541,8.541,0,0,0,8.516,8.516H26.548V63.613H9.516ZM52.1,38.064H35.064v7.579a12.622,12.622,0,0,0,0,18.906v7.579H52.1V64.55a12.622,12.622,0,0,0,0-18.906ZM43.581,61.484A6.387,6.387,0,1,1,49.968,55.1,6.379,6.379,0,0,1,43.581,61.484ZM90.419,21.032H64.871a4.579,4.579,0,0,0-4.258,4.258V67.871a4.579,4.579,0,0,0,4.258,4.258H90.419a4.579,4.579,0,0,0,4.258-4.258V25.29A4.579,4.579,0,0,0,90.419,21.032ZM86.161,63.613H69.129V29.548H86.161Z" transform="translate(-1 -4)"/>
                   </svg>
                 </Nav.Link>
                 <Nav.Link as={Link} to="contact" onClick={() => this.props.setCurrPage('Contact')}>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="41" height="41" viewBox="0 0 68.129 68.129">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="41" height="41" viewBox="0 0 68.129 68.129" alt="Link to the Contact page">
                     <path id="contact-icon" d="M66.723,15.626H59.91V46.284H15.626V53.1A3.416,3.416,0,0,0,19.032,56.5H56.5L70.129,70.129v-51.1A3.416,3.416,0,0,0,66.723,15.626ZM53.1,36.064V5.406A3.416,3.416,0,0,0,49.69,2H5.406A3.416,3.416,0,0,0,2,5.406V53.1L15.626,39.471H49.69A3.416,3.416,0,0,0,53.1,36.064Z" transform="translate(-2 -2)"/>
                   </svg>
                 </Nav.Link>
