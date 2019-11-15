a519c6be364603086ebdb5eaf799ce1cfe7da4b2
Brennan Newton <brennanmnewton@gmail.com>
2019-11-14 19:27:29 -0800

    Pagination & finish mv styling

---
+++ react-bndesign/src/components/js/MVPagination.js
@@ -0,0 +1,54 @@
+// Components
+import React from 'react';
+import Pagination from 'react-bootstrap/Pagination';
+
+export default class MVPagination extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showPg: 1,
+    };
+  }
+
+  setPage = (num) => {
+    this.setState({
+      showPg: num,
+    });
+  }
+
+  render() {
+    if(this.state.showPg === 1) {
+      return (
+        <>
+        <div>
+          <iframe src="https://www.youtube.com/embed/Yd04u_iSzXs" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+          <iframe src="https://www.youtube.com/embed/G3rbz1-R8B0" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+          <iframe src="https://www.youtube.com/embed/pjKZb9llaNQ" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        </div>
+        <Pagination size="sm" className="justify-content-center">
+          {[
+            <Pagination.Item key={1} onClick={() => this.setPage(1)}>1</Pagination.Item>,
+            <Pagination.Item key={2} onClick={() => this.setPage(2)}>2</Pagination.Item>
+          ]}
+        </Pagination>
+        </>
+      )
+    }
+    else {
+      return (
+        <>
+        <div>
+          <iframe src="https://www.youtube.com/embed/Hx60dvg0oFw" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+          <iframe src="https://www.youtube.com/embed/Jqyy7i3Asao" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        </div>
+        <Pagination size="sm" className="justify-content-center">
+          {[
+            <Pagination.Item key={1} onClick={() => this.setPage(1)}>1</Pagination.Item>,
+            <Pagination.Item key={2} onClick={() => this.setPage(2)}>2</Pagination.Item>
+          ]}
+        </Pagination>
+        </>
+      )
+    }
+  }
+}
--- react-bndesign/src/components/js/MusicVideos.js
+++ react-bndesign/src/components/js/MusicVideos.js
@@ -2,43 +2,49 @@
 import React from 'react';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-import Pagination from 'react-bootstrap/Pagination';
+import MVPagination from './MVPagination';
 
 //Styles
 import '../css/MusicVideos.css'
 
 export default class MusicVideos extends React.Component {
-  handleClick1 = () => {
-    console.log("Page 1 clicked");
-  }
-
-  handleClick2 = () => {
-    console.log("Page 2 clicked");
-  }
-
   render () {
     return (
       <div className="MusicVideos-Component">
         <Row className="justify-content-center w-100">
-          <Col xs={8} lg={5} className="justify-content-center mr-5 mb-5 ml-5">
+          <Col xs={8} lg={5} className="justify-content-center mt-3 mr-5 mb-5 ml-5">
             <div className="mv-container m-auto">
-              <iframe src="https://www.youtube.com/embed/Yd04u_iSzXs" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-              <iframe src="https://www.youtube.com/embed/G3rbz1-R8B0" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-              <iframe src="https://www.youtube.com/embed/pjKZb9llaNQ" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-              <Pagination size="sm" className="justify-content-center">
-                  {[
-                    <Pagination.Item key={1} onClick={this.handleClick1}>1</Pagination.Item>,
-                    <Pagination.Item key={2} onClick={this.handleClick2}>2</Pagination.Item>
-                  ]}
-              </Pagination>
+              <MVPagination />
             </div>
           </Col>
           <Col xs={8} lg={5} className="justify-content-center mr-5 ml-5">
             <h1>Music Videos</h1>
-            <p>My videos investigate the relationship between music & emotion. Music makes us laugh. Music makes us cry. Music scares us.</p>
-            <iframe id="30c936b7-b733-4ea9-9464-2fad0bf27a3a" src="https://www.vectary.com/viewer/v1/?model=30c936b7-b733-4ea9-9464-2fad0bf27a3a&env=studio1" frameBorder="0" width="33%" height="480"></iframe>
-            <iframe id="30c936b7-b733-4ea9-9464-2fad0bf27a3a" src="https://www.vectary.com/viewer/v1/?model=30c936b7-b733-4ea9-9464-2fad0bf27a3a&env=studio1" frameBorder="0" width="33%" height="480"></iframe>
-            <iframe id="30c936b7-b733-4ea9-9464-2fad0bf27a3a" src="https://www.vectary.com/viewer/v1/?model=30c936b7-b733-4ea9-9464-2fad0bf27a3a&env=studio1" frameBorder="0" width="33%" height="480"></iframe>
+            <p>
+              Because they take up both time & space, videos can be used to convey complex ideas.
+              <br />
+              Bring music into the mix, and you have a way to portray complex emotions.
+            </p>
+              <iframe 
+                id="30c936b7-b733-4ea9-9464-2fad0bf27a3a" 
+                src="https://www.vectary.com/viewer/v1/?model=30c936b7-b733-4ea9-9464-2fad0bf27a3a&env=studio1" 
+                frameBorder="0" 
+                width="33%" 
+                height="480"
+              ></iframe>
+              <iframe 
+                id="30c936b7-b733-4ea9-9464-2fad0bf27a3a" 
+                src="https://www.vectary.com/viewer/v1/?model=30c936b7-b733-4ea9-9464-2fad0bf27a3a&env=studio1" 
+                frameBorder="0" 
+                width="33%" 
+                height="480"
+              ></iframe>
+              <iframe 
+                id="30c936b7-b733-4ea9-9464-2fad0bf27a3a" 
+                src="https://www.vectary.com/viewer/v1/?model=30c936b7-b733-4ea9-9464-2fad0bf27a3a&env=studio1" 
+                frameBorder="0" 
+                width="33%" 
+                height="480"
+              ></iframe>
           </Col>
         </Row>
       </div>
