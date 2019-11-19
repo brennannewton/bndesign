c8ff8d5f27022889eb4fda35c261f0273d1b73ce
Brennan Newton <brennanmnewton@gmail.com>
2019-11-19 14:34:47 -0800

    UX page redesign

--- react-bndesign/src/components/js/UX.js
+++ react-bndesign/src/components/js/UX.js
@@ -1,61 +1,20 @@
 // Components
 import React from 'react';
-import Carousel from 'react-bootstrap/Carousel';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import UXPagination from './UXPagination';
 
 // Styles
 import '../css/UX.css';
-
-// Images
-
-import ELECTRA from '../../img/ELECTRA_SS_Fade.png';
-import bndesign from '../../img/bndesign_SS_Fade.png';
 
 export default class UX extends React.Component {
   render() {
     return (
       <div className="UX-Component ml-4">
-        <h1 className="mb-4">UX</h1>
+        <h1 className="mr-4">UX</h1>
         <Row className="w-100">
-          <Col className="mb-4">
-            <Carousel>
-              <Carousel.Item>
-                <img
-                  className="d-block w-100"
-                  src={ELECTRA}
-                  alt="First slide"
-                />
-                <Carousel.Caption>
-                  <h3>ELECTRA</h3>
-                  <p>ELECTRA provides collaboration tools for the modern artist.</p>
-                </Carousel.Caption>
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                  className="d-block w-100"
-                  src={bndesign}
-                  alt="Second slide"
-                />
-                <Carousel.Caption>
-                  <h3>bndesign</h3>
-                  <p>bndesign serves as my personal portfolio as well as a UX project.</p>
-                </Carousel.Caption>
-              </Carousel.Item>
-            </Carousel>
-          </Col>
-          <Col className="w-100 d-flex flex-row">
-            <div className="link-container mr-5 ml-3">
-              <h3>Sites</h3>
-              <p><a href="https://electrala.netlify.com" target="_blank">electrala.netlify.com</a></p>
-              <p><a href="https://bndesgin.netlify.com" target="_blank">bndesign.netlify.com</a></p>
-            </div>
-            <div className="code-container">
-              <h3>Code</h3>
-              <p><a href="https://github.com/electrala" target="_blank">ELECTRA</a></p>
-              <p><a href="https://github.com/brennannewton/bndesign" target="_blank">bndesign</a></p>
-            </div>
-          </Col>
+          <div className="project-container w-100 m-5 p-4">
+            <UXPagination />
+          </div>
         </Row>
       </div>
     )
---
+++ react-bndesign/src/components/js/UXPagination.js
@@ -0,0 +1,104 @@
+// Components
+import React from 'react';
+import Pagination from 'react-bootstrap/Pagination';
+import Viewer from 'react-viewer';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+// Images
+import bndesign from '../../img/bndesign_SS.png';
+import bndesign_home from '../../img/bndesign_wireframes/Home.png';
+import bndesign_mv from '../../img/bndesign_wireframes/MusicVideos.png';
+import bndesign_illustrations from '../../img/bndesign_wireframes/Illustrations.png';
+import bndesign_ux from '../../img/bndesign_wireframes/UX.png';
+import bndesign_contact from '../../img/bndesign_wireframes/Contact.png';
+
+// Styles
+import '../css/UXPagination.css';
+
+export default class MVPagination extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showPg: 1,
+      visible: false,
+    };
+  }
+
+  setPage = (num) => {
+    this.setState({
+      showPg: num,
+    });
+  }
+
+  render() {
+    if(this.state.showPg === 1) {
+      return (
+        <div className="UX-project">
+          <a href="https://bndesign.netlify.com/" target="_blank">
+            <h2 className="title mb-4">bndesign</h2>
+          </a>
+          <Row>
+              <Col>
+                <div className="viewer_bndesign mb-4">
+                  <button onClick={() => { this.setState({ visible: true }); }}>
+                    <img className="w-100" src={ bndesign } alt="bndesign screenshot" />
+                  </button>
+                  <Viewer
+                    visible={this.state.visible}
+                    onClose={() => { this.setState({ visible: false }); }}
+                    images={[
+                      { src: bndesign_home, alt: "Home" },
+                      { src: bndesign_mv, alt: "Music Videos" },
+                      { src: bndesign_illustrations, alt: "Illustrations" },
+                      { src: bndesign_ux, alt: "UX" },
+                      { src: bndesign_contact, alt: "Contact" }
+                    ]}
+                    noImgDetails={true}
+                  />
+                </div>
+              </Col>
+              <Col className="mr-4 mb-4">
+                <p className="call-to-action">Click the screenshot to be taken to the live site!</p>
+                <p className="description">
+                  <b>bndesign</b> is both a portfolio for my work and a user
+                  experience project. My goal is to create a welcoming space
+                  where recruiters, hiring managers, and enthusiasts can
+                  explore my digital content. Soft, minimal design gives the
+                  site space, which allows the user to easily focus on the
+                  content. Contrast that with metalic 3D objects to create
+                  dimensionality that grabs the user's attention. These
+                  elements come together to highlight my work.
+                </p>
+                <a href="https://github.com/brennannewton/bndesign" target="_blank">
+                  <p className="repo-link">GitHub Repository</p>
+                </a>
+              </Col>
+          </Row>
+          <Pagination size="sm" className="justify-content-center">
+            {[
+              <Pagination.Item key={1} onClick={() => this.setPage(1)}>1</Pagination.Item>,
+              <Pagination.Item key={2} onClick={() => this.setPage(2)}>2</Pagination.Item>
+            ]}
+          </Pagination>
+        </div>
+      )
+    }
+    else {
+      return (
+        <>
+        <div>
+          <iframe src="https://www.youtube.com/embed/Hx60dvg0oFw" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+          <iframe src="https://www.youtube.com/embed/Jqyy7i3Asao" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        </div>
+        <Pagination size="sm" className="justify-content-center">
+          {[
+            <Pagination.Item key={1} onClick={() => this.setPage(1)}>1</Pagination.Item>,
+            <Pagination.Item key={2} onClick={() => this.setPage(2)}>2</Pagination.Item>
+          ]}
+        </Pagination>
+        </>
+      )
+    }
+  }
+}
