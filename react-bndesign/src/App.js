85d73cdf9964f199999b0ae2a338f45ca8869785
Brennan Newton <brennanmnewton@gmail.com>
2019-11-21 09:43:08 -0800

    MV responsive styling

--- react-bndesign/src/App.js
+++ react-bndesign/src/App.js
@@ -41,7 +41,7 @@ export default class App extends React.Component {
             className="switch-wrapper"
           >
             <Route path="/" exact render={() => <Home />} />
-            <Route path="/music-videos" render={() => <MusicVideos />} />
+            <Route path="/music-videos" exact render={() => <MusicVideos />} />
             <Route path="/illustrations" render={() => <Illustrations />} />
             <Route path="/paintings" render={() => <Paintings />} />
             <Route path="/ux" render={() => <UX />} />
--- react-bndesign/src/components/js/MusicVideos.js
+++ react-bndesign/src/components/js/MusicVideos.js
@@ -12,22 +12,28 @@ export default class MusicVideos extends React.Component {
     return (
       <div className="MusicVideos-Component">
         <Row className="justify-content-center w-100">
-          <Col xs={8} lg={5} className="justify-content-center mt-3 mr-5 mb-5 ml-5">
+          <Col xs={11} md={8} xl={5} className="justify-content-center mt-3 mr-5 mb-5 ml-5">
             <div className="mv-container m-auto">
               <MVPagination />
             </div>
           </Col>
-          <Col xs={8} lg={5} className="justify-content-center mt-2 mr-5 ml-5">
+          <Col xs={11} xl={5} className="justify-content-center mt-2 mr-5 ml-5">
             <h1>Music Videos</h1>
-            <p>
+            <p style={{ marginBottom:0 }}>
               Because they take up both time & space, videos can be used to convey complex ideas.
               Bring music into the mix, and now you have a way to portray complex emotions.
             </p>
-            <div className="mt-3">
-              <iframe id="30c936b7-b733-4ea9-9464-2fad0bf27a3a" src="https://www.vectary.com/viewer/v1/?model=30c936b7-b733-4ea9-9464-2fad0bf27a3a&env=redwall" frameBorder="0" width="33%" height="480"></iframe>
-              <iframe id="30c936b7-b733-4ea9-9464-2fad0bf27a3a" src="https://www.vectary.com/viewer/v1/?model=30c936b7-b733-4ea9-9464-2fad0bf27a3a&env=redwall" frameBorder="0" width="33%" height="480"></iframe>
-              <iframe id="30c936b7-b733-4ea9-9464-2fad0bf27a3a" src="https://www.vectary.com/viewer/v1/?model=30c936b7-b733-4ea9-9464-2fad0bf27a3a&env=redwall" frameBorder="0" width="33%" height="480"></iframe>
-            </div>
+            <Row className="mt-3">
+              <Col xs={12} md={4} className="p-0">
+                 <iframe id="30c936b7-b733-4ea9-9464-2fad0bf27a3a" src="https://www.vectary.com/viewer/v1/?model=30c936b7-b733-4ea9-9464-2fad0bf27a3a&env=redwall" frameBorder="0" width="100%" height="480"></iframe>
+              </Col>
+              <Col xs={12} md={4} className="p-0">
+                <iframe id="30c936b7-b733-4ea9-9464-2fad0bf27a3a" src="https://www.vectary.com/viewer/v1/?model=30c936b7-b733-4ea9-9464-2fad0bf27a3a&env=redwall" frameBorder="0" width="100%" height="480"></iframe>
+              </Col>
+              <Col xs={12} md={4} className="p-0">
+                <iframe id="30c936b7-b733-4ea9-9464-2fad0bf27a3a" src="https://www.vectary.com/viewer/v1/?model=30c936b7-b733-4ea9-9464-2fad0bf27a3a&env=redwall" frameBorder="0" width="100%" height="480"></iframe>
+              </Col>
+            </Row>
           </Col>
         </Row>
       </div>
